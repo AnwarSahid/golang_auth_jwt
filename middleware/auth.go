11b91557a4180d4b7e8fc007fc779636732356fb
AnwarSahid <anwarsahid28oktober@gmail.com>
2023-08-20 23:22:11 +0700

    add verifyToken

---
+++ middleware/auth.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"golang_jwt/helpers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authorization() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		verifyToken, err := helpers.VerifyToken(c)
+		_ = verifyToken
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error":   "Unauthorized",
+				"message": err.Error(),
+			})
+			return
+		}
+		c.Set("userdata", verifyToken)
+		c.Next()
+	}
+}
